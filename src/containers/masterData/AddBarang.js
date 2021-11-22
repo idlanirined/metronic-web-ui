5d1ffecfc893f55e6e07c46db9b4721c31608332
Deni <deni@gmail.com>
2021-11-22 13:05:28 +0700

    barang

---
+++ src/containers/masterData/AddBarang.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import CloseImage from '../../assets/ic_close.png';
+import { Autocomplete, TextField, Typography } from "@mui/material";
+
+export default function AddBarang(props) {
+    const [listGolongan, setlistGolongan] = React.useState([
+        { id: 1, value: 'OPEX' },
+        { id: 2, value: 'CAPEX' },
+    ])
+    const [listPerlengkapan, setListPerlengkapan] = React.useState([
+        { id: 1, value: 'Perlengkapan Kantor' },
+        { id: 2, value: 'ATK' },
+    ])
+    const [listSatuan, setListSatuan] = React.useState([
+        { id: 1, value: 'RIM' }
+    ])
+
+    return (
+        <div className="App app-popup-show">
+            <div className="popup-content-middle background-white" style={{ borderRadius: 8, backgroundColor: 'white' }}>
+                <div className="popup-panel grid grid-2x" style={{ height: 64 }}>
+                    <div className="col-1" style={{ maxWidth: "inherit", display: 'flex', alignItems: 'center' }}>
+                        <div className="popup-title">
+                            <span style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>Tambah Barang</span>
+                        </div>
+                    </div>
+                    <div className="col-2 content-right" style={{ maxWidth: "inherit", alignSelf: 'center' }}>
+                        <button
+                            type="button"
+                            className="btn btn-circle btn-white"
+                            onClick={() => props.onClose()}
+                        >
+                            <img src={CloseImage} style={{ width: 20, height: 20 }} />
+                        </button>
+                    </div>
+                </div>
+                <div className="grid grid-2x grid-mobile-none gap-15px" style={{ padding: 20, paddingRight: 50, paddingLeft: 50 }}>
+                    <div className="column-1" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Nama Barang</Typography>
+                        <TextField
+                            style={{ width: '100%' }}
+                            variant="outlined"
+                            onChange={(e) => null}
+                            inputProps={{
+                                style: {
+                                    fontSize: 14,
+                                    backgroundColor: '#e5e5e5'
+                                }
+                            }}
+                            size="medium"
+                            InputLabelProps={{
+                                style: {
+                                    fontSize: 14,
+                                    color: '#7e8085',
+                                }
+                            }}
+                        />
+                    </div>
+                    <div className="column-2" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Golongan Barang</Typography>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={listGolongan}
+                            getOptionLabel={(option) => option.value}
+                            sx={{ width: 'inherit' }}
+                            style={{
+                                fontSize: 14,
+                                backgroundColor: '#e5e5e5'
+                            }}
+                            renderInput={(params) =>
+                                <TextField {...params} />}
+                        />
+                    </div>
+                    <div className="column-1" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Jenis Barang</Typography>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={listPerlengkapan}
+                            getOptionLabel={(option) => option.value}
+                            sx={{ width: 'inherit' }}
+                            style={{
+                                fontSize: 14,
+                                backgroundColor: '#e5e5e5'
+                            }}
+                            renderInput={(params) =>
+                                <TextField {...params} />}
+                        />
+                    </div>
+                    <div className="column-2" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Satuan Barang</Typography>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={listSatuan}
+                            getOptionLabel={(option) => option.value}
+                            sx={{ width: 'inherit' }}
+                            style={{
+                                fontSize: 14,
+                                backgroundColor: '#e5e5e5'
+                            }}
+                            renderInput={(params) =>
+                                <TextField {...params} />}
+                        />
+                    </div>
+                    <div className="column-1" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Merk</Typography>
+                        <TextField
+                            style={{ width: '100%' }}
+                            variant="outlined"
+                            onChange={(e) => null}
+                            inputProps={{
+                                style: {
+                                    fontSize: 14,
+                                    backgroundColor: '#e5e5e5'
+                                }
+                            }}
+                            size="medium"
+                            InputLabelProps={{
+                                style: {
+                                    fontSize: 14,
+                                    color: '#7e8085',
+                                }
+                            }}
+                        />
+                    </div>
+                    <div className="column-2" style={{ display: 'flex' }}>
+                        <Typography style={{ textAlign: 'left', fontWeight: 'bold', alignSelf: 'center', width: 130 }}>Account</Typography>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={listPerlengkapan}
+                            getOptionLabel={(option) => option.value}
+                            sx={{ width: 'inherit' }}
+                            style={{
+                                fontSize: 14,
+                                backgroundColor: '#e5e5e5'
+                            }}
+                            renderInput={(params) =>
+                                <TextField {...params} />}
+                        />
+                    </div>
+                </div>
+                <div className="grid grid-2x grid-mobile-none gap-15px" style={{ padding: 20, paddingRight: 50, paddingLeft: 50 }}>
+                    <div>
+
+                    </div>
+                    <div style={{ marginTop: 50, justifySelf: 'flex-end' }}>
+                        <div style={{ height: 60, width: 250, backgroundColor: '#3699ff', display: 'flex', justifyContent: 'center', borderRadius: 10 }}>
+                            <Typography style={{ color: 'white', fontSize: 18, fontWeight: 'bold', textAlign: 'center', alignSelf: 'center' }}>TAMBAH</Typography>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
